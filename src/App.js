cace19e724f27337dbd24f84fa90ffa93ffc8489
Ali Hassan <contact@alihassan.dev>
2019-12-23 11:40:36 +0500

    added react-helmet package

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>React Project Biolerplate</title>
+      </Helmet>
+      Test
+    </div>
+  );
 }
 
 export default App;
