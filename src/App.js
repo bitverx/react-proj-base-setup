e2ee0c8ab2976a7f97c7195d3b69ca43cd39bf40
Ali Hassan <contact@alihassan.dev>
2019-12-23 11:12:06 +0500

    added sass support

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./style/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          asdas Edit <code>src/App.js</code> and save to reload.
-        </p>
-        asdasdasd
-        <a
-          asasdasdasd
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactsdasdas
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Test</div>;
 }
 
 export default App;
